c8ee33e08749f8893637ddb3fa3182eecf0dcee3
Bradley Burgess <bradleyburgess@gmail.com>
2022-04-23 10:34:50 +0200

    Added `loggingLevel` to options object.
    Also slight refactor of validating user options

--- .eleventy.js
+++ .eleventy.js
@@ -2,66 +2,64 @@ const chalk = require("chalk");
 const store = require("./lib/store");
 const getExternalLinksFromPage = require("./lib/getExternalLinksFromPage");
 const checkLinkStatuses = require("./lib/checkLinkStatuses");
-const { log, isBroken, isRedirect, indent, bullet } = require("./lib/helpers");
+const { isBroken, isRedirect } = require("./lib/helpers");
+const validateUserOptions = require("./lib/validateUserOptions");
+const log = require("./lib/logger");
 
 module.exports = function (eleventyConfig, _options) {
   const defaults = {
     broken: "warn",
     redirect: "warn",
     cacheDuration: "1d",
+    loggingLevel: 2,
   };
+
+  // validate user-supplied options
+  validateUserOptions(_options);
 
   // merge default and user options
   const options = { ...defaults, ..._options };
+  options.loggingLevel = parseInt(options.loggingLevel);
 
   // "Lint" each page and gather links
-  eleventyConfig.addLinter(
-    "getExternalLinksFromPage",
-    getExternalLinksFromPage
-  );
+  eleventyConfig.addLinter("getExternalLinksFromPage", getExternalLinksFromPage);
 
   eleventyConfig.on("eleventy.after", async () => {
-    await checkLinkStatuses();
+    await checkLinkStatuses(options.cacheDuration);
     const allLinks = Object.keys(store);
-    const brokenLinks = allLinks.filter((link) =>
-      isBroken(store[link].getHttpStatusCode())
-    );
-    const redirectLinks = allLinks.filter((link) =>
-      isRedirect(store[link].getHttpStatusCode())
-    );
+    const brokenLinks = allLinks.filter((link) => isBroken(store[link].getHttpStatusCode()));
+    const redirectLinks = allLinks.filter((link) => isRedirect(store[link].getHttpStatusCode()));
     const okayLinks = allLinks.filter(
       (link) =>
-        !isBroken(store[link].getHttpStatusCode()) &&
-        !isRedirect(store[link].getHttpStatusCode())
+        !isBroken(store[link].getHttpStatusCode()) && !isRedirect(store[link].getHttpStatusCode())
     );
 
-    okayLinks.forEach((link) => {
-      console.log(chalk.green(`Link okay:      ${link}`));
-    });
+    options.loggingLevel === 3 &&
+      okayLinks.forEach((link) => {
+        log().okay(`Link okay:      ${link}`);
+      });
 
-    redirectLinks.forEach((link) => {
-      const pages = store[link].getPages();
-      log.warn(`Link redirects: ${link}`);
-      log.normal(indent("Used on these pages:", 2));
-      pages.forEach((page) => log.normal(indent(bullet(page))));
-    });
+    options.loggingLevel >= 2 &&
+      redirectLinks.forEach((link) => {
+        const pages = store[link].getPages();
+        log().warn(`Link redirects: ${link}`);
+        log().display(`Used ${store[link].getLinkCount()} time(s) on these pages:`, 2);
+        pages.forEach((page) => log().bullet().indent().display(page));
+      });
 
-    brokenLinks.forEach((link) => {
-      const pages = store[link].getPages();
-      log.error(`Link is broken: ${link}`);
-      log.normal(indent("Used on these pages:", 2));
-      pages.forEach((page) => log.normal(indent(bullet(page))));
-    });
+    options.loggingLevel >= 1 &&
+      brokenLinks.forEach((link) => {
+        const pages = store[link].getPages();
+        log().error().display(`Link is broken: ${link}`);
+        log().display(`Used ${store[link].getLinkCount()} time(s) on these pages:`, 2);
+        pages.forEach((page) => log().bullet().indent().display(page));
+      });
 
     if (options.broken === "error" && brokenLinks.length > 0)
-      throw new Error(
-        "There are broken links in your build! See above for details."
-      );
+      throw new Error("There are broken links in your build! See above for details.");
     if (options.redirect === "error" && redirectLinks.length > 0)
-      throw new Error(
-        "There are redirect links in your build! See above for details."
-      );
+      throw new Error("There are redirect links in your build! See above for details.");
   });
 };
 
-const nope = "no"
+const nope = "no";
